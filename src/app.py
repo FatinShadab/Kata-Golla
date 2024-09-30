3955c4911f0fd8d0e6655e58ecb3e9461807592b
Fatin Shadab <71595077+FatinShadab@users.noreply.github.com>
2024-09-30 21:51:51 +0600

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,5 +1,7 @@
 import tkinter as tk
+import asyncio
 
+from ai import Agent
 
 class Game:
     TITLE = "XO কাট-গোল্লা"
@@ -12,17 +14,25 @@ class Game:
         "grid_cell": "#3C3D37",
         "border": "#181C14",
         "text": "#F2EFE9",
+        "win": "#00ab66"
     }
 
-    def __init__(self, player_x="X", player_o="O"):
-        self.player_x_marker = player_x
-        self.player_o_marker = player_o
+    def __init__(self, ai=True, ai_level=0):
+        self.ai_enabled = ai
+        self.ai_level = ai_level
+        self.player_x_marker = "❌"
+        self.player_o_marker = "⭕"
         self.current_player = self.player_x_marker
+        
+        if self.ai_enabled:
+            self.agent = Agent(ai_level, self.player_o_marker, self.player_x_marker)
+            
         self.board = [
             [None, None, None],
             [None, None, None],
             [None, None, None]
         ]
+        self.board_state_active = True
         self.win_condition = 3
         self.moves = 0
 
@@ -43,34 +53,93 @@ class Game:
         for row in range(3):
             for column in range(3):
                 self.board[row][column].config(text="", state="normal")
+                self.board[row][column].config(background=Game.COLOR_SHEMA["grid_cell"])
         self.current_player = self.player_x_marker
         self.current_player_label.config(text=f"Current Player: {self.current_player}")
         self.moves = 0
+        self.board_state_active = True
+
+    def __alter_board_state(self):
+        self.board_state_active = not self.board_state_active
+        
+        for row in self.board:
+            for cell in row:
+                cell.config(state="disabled" if not self.board_state_active else "normal")
 
     def __handle_user_click(self, row, column):
-        # Handle player move
         if self.board[row][column].cget('text') == "":
             self.board[row][column].config(text=self.current_player)
+            self.board[row][column].update_idletasks()  # Update the board to show the player's move
             self.moves += 1
+
             if self.__check_winner():
                 self.__end_game(f"Player {self.current_player} wins!")
-            elif self.moves == 9:
+            elif self.moves >= 9:
                 self.__end_game("It's a draw!")
             else:
-                self.current_player = self.player_o_marker if self.current_player == self.player_x_marker else self.player_x_marker
-                self.current_player_label.config(text=f"Current Player: {self.current_player}")
+                self.__alter_player_marker()
+                
+                # Implement AI move
+                if self.ai_enabled:
+                    self.__alter_board_state()
+                    asyncio.run(self.__ai_move())
+                    self.__alter_board_state()
 
+    async def __ai_move(self):
+        self.agent.make_move(self.board)
+        self.moves += 1
+
+        if self.__check_winner():
+            self.__end_game(f"Player {self.current_player} wins!")
+        elif self.moves >= 9:
+            self.__end_game("It's a draw!")
+        else:
+            self.__alter_player_marker()
+
+    def __alter_player_marker(self):
+        self.current_player = self.player_o_marker if self.current_player == self.player_x_marker else self.player_x_marker
+        self.current_player_label.config(text=f"Current Player: {self.current_player}")
+        self.current_player_label.update_idletasks()  # Update the label
+    
     def __check_winner(self):
         # Check rows, columns, and diagonals for a win
         for i in range(3):
             if self.board[i][0].cget('text') == self.board[i][1].cget('text') == self.board[i][2].cget('text') != "":
+                self.__color_cells(
+                    [self.board[i][0], self.board[i][1], self.board[i][2]],
+                    Game.COLOR_SHEMA["win"]
+                )
+                self.__alter_board_state()
+                
                 return True
+            
             if self.board[0][i].cget('text') == self.board[1][i].cget('text') == self.board[2][i].cget('text') != "":
+                self.__color_cells(
+                    [self.board[0][i], self.board[1][i], self.board[2][i]],
+                    Game.COLOR_SHEMA["win"]
+                )
+                self.__alter_board_state()
+                
                 return True
+            
         if self.board[0][0].cget('text') == self.board[1][1].cget('text') == self.board[2][2].cget('text') != "":
+            self.__color_cells(
+                [self.board[0][0], self.board[1][1], self.board[2][2]],
+                Game.COLOR_SHEMA["win"]
+            )
+            self.__alter_board_state()
+            
             return True
+        
         if self.board[0][2].cget('text') == self.board[1][1].cget('text') == self.board[2][0].cget('text') != "":
+            self.__color_cells(
+                [self.board[0][2], self.board[1][1], self.board[2][0]],
+                Game.COLOR_SHEMA["win"]
+            )
+            self.__alter_board_state()
+            
             return True
+        
         return False
 
     def __end_game(self, message):
@@ -118,9 +187,15 @@ class Game:
     def __gameloop(self):
         self.window.mainloop()
 
+    def __color_cells(self, cells, color):
+        for cell in cells:
+            cell.config(background=color)
+
     def run(self):
         self.__gameloop()
 
 
 if __name__ == "__main__":
-    Game().run()
+    ai_enabled = True
+    ai_level = 2
+    Game(ai=ai_enabled, ai_level=ai_level).run()
